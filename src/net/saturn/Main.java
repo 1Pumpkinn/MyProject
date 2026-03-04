ae92cba234f984951da75d4c2542a7c4ac7edc65
tyron <wishsmp1002@gmail.com>
2026-03-04 21:44:06 +1100

    Switch Statement

--- src/net/saturn/Main.java
+++ src/net/saturn/Main.java
@@ -1,17 +1,27 @@
 package net.saturn;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
+        // Switch statements are basically a way to easily format a load of if else's
+        // break is going to jump out of the current code block and execute any other code below
+        // you can add a break to a for loop, while loop, or wherever and that will jump out of the loop and continue the code.
+        // return actually stops the code execution as a whole
 
-        SecondClass.calculate();
+        String word = "banana";
 
+        switch (word) {
+            case "banana": System.out.println("You found it!"); return;
 
+            case "apple": System.out.println("This is not a banana"); break;
+            case "pear": System.out.println("This is not a banana"); break;
+            case "kiwi": System.out.println("This is not a banana"); break;
+            default: System.out.println("No Match was found!"); break;
+            // if user puts in something that we don't account for
+        }
 
-
-
+        System.out.println("Hello you broke out the code block");
 
     }
 }
